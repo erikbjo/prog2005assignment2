cd0ca7adc2fcb4fbf3ecc5c8bc854ca5976e5b95
Simon <106014761+simonhoumb@users.noreply.github.com>
2024-04-09 11:44:01 +0200

    feat: added function to create custom ID

--- server/utils/id_utils.go
+++ server/utils/id_utils.go
@@ -1,12 +1,17 @@
 package utils
 
 import (
+	"crypto/sha256"
 	"fmt"
 	"log"
+	"math/rand"
 	"net/http"
 	"strconv"
+	"strings"
+	"time"
 )
 
+// GetIDFromRequest returns the ID provided in the request URL
 func GetIDFromRequest(r *http.Request) (int, error) {
 	id := r.PathValue("id")
 	if id == "" {
@@ -21,4 +26,45 @@ func GetIDFromRequest(r *http.Request) (int, error) {
 	}
 
 	return idInt, nil
+}
+
+// Function to generate a random string of specified length
+func generateRandomString(length int) string {
+	const charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+	seededRand := rand.New(rand.NewSource(time.Now().UnixNano()))
+	result := make([]byte, length)
+	for i := range result {
+		result[i] = charset[seededRand.Intn(len(charset))]
+	}
+	return string(result)
+}
+
+// GenerateRandomID Function to generate custom 8 character ID
+func GenerateRandomID() string {
+	// Generate random string
+	randomString := generateRandomString(10)
+
+	// Generate timestamp as salt
+	timestamp := strconv.FormatInt(time.Now().Unix(), 10)
+
+	// Concatenate random string and timestamp
+	concatenated := randomString + timestamp
+
+	// Hash the concatenated string using SHA-256
+	hash := sha256.New()
+	hash.Write([]byte(concatenated))
+	hashed := hash.Sum(nil)
+
+	// Convert hashed bytes to hexadecimal string
+	customID64 := fmt.Sprintf("%x", hashed)
+
+	// Uses builder to get every 8th character from customID64, resulting in only 8 characters
+	var customIDBuilder strings.Builder
+	for i := 0; i < len(customID64); i++ {
+		if i%8 == 0 {
+			customIDBuilder.WriteRune(rune(customID64[i]))
+		}
+	}
+
+	return customIDBuilder.String()
 }
