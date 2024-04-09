035b7318c36f46da724ef3c83ce65ccb8b21fcf9
Erik Bjørnsen <bjornsen.erik@gmail.com>
2024-04-09 12:58:27 +0200

    chore: changed id from int to string

--- server/utils/id_utils.go
+++ server/utils/id_utils.go
@@ -4,21 +4,15 @@ import (
 	"fmt"
 	"log"
 	"net/http"
-	"strconv"
 )
 
-func GetIDFromRequest(r *http.Request) (int, error) {
+// GetIDFromRequest Get the ID from the request
+func GetIDFromRequest(r *http.Request) (string, error) {
 	id := r.PathValue("id")
 	if id == "" {
 		log.Println("ID not provided")
-		return -1, fmt.Errorf("ID not provided")
+		return "", fmt.Errorf("ID not provided")
 	}
 
-	idInt, err := strconv.Atoi(id)
-	if err != nil {
-		log.Println("Invalid ID, must be an integer")
-		return -1, fmt.Errorf("invalid ID, must be an integer")
-	}
-
-	return idInt, nil
+	return id, nil
 }
