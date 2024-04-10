e9bf65e235fcffc1bdb423115fb9ecfd907fc4cc
Erik Bjørnsen <bjornsen.erik@gmail.com>
2024-04-10 14:35:16 +0200

    chore: added meteo api response struct

--- server/shared/structs.go
+++ server/shared/structs.go
@@ -49,6 +49,23 @@ type Dashboard struct {
 	LastRetrieval string `json:"lastRetrieval"`
 }
 
+type MeteoForecastResponse struct {
+	Latitude             float64 `json:"latitude"`
+	Longitude            float64 `json:"longitude"`
+	Elevation            float64 `json:"elevation"`
+	GenerationtimeMs     float64 `json:"generationtime_ms"`
+	UtcOffsetSeconds     int     `json:"utc_offset_seconds"`
+	Timezone             string  `json:"timezone"`
+	TimezoneAbbreviation string  `json:"timezone_abbreviation"`
+	Hourly               struct {
+		Time          []interface{} `json:"time"`
+		Temperature2M []interface{} `json:"temperature_2m"`
+	} `json:"hourly,omitempty"`
+	HourlyUnits struct {
+		Temperature2M string `json:"temperature_2m"`
+	} `json:"hourly_units,omitempty"`
+}
+
 type SiteMap struct {
 	Help      string     `json:"help"`
 	Endpoints []Endpoint `json:"siteMap"`
