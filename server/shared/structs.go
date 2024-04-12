d9c01308fd875946ca63b0a4923095a86effc271
Simon <106014761+simonhoumb@users.noreply.github.com>
2024-04-12 14:20:28 +0200

    feat: added Notification struct

--- server/shared/structs.go
+++ server/shared/structs.go
@@ -58,6 +58,13 @@ type Dashboard struct {
 	LastRetrieval string `json:"lastRetrieval"`
 }
 
+type Notification struct {
+	Id      string    `json:"id"`
+	Country string    `json:"country"`
+	Event   string    `json:"event"`
+	Time    time.Time `json:"time"`
+}
+
 type MeteoForecastResponse struct {
 	Latitude             float64 `json:"latitude"`
 	Longitude            float64 `json:"longitude"`
