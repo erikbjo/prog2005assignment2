94ccd0e1e5c89e1504233ad589b86829f30feff7
Simon <simon.houmb.2302@gmail.com>
2024-04-15 21:14:27 +0200

    feat: added notificationResponse struct

--- internal/http/handlers/notifications/notifications_shared.go
+++ internal/http/handlers/notifications/notifications_shared.go
@@ -2,14 +2,10 @@ package notifications
 
 import (
 	"assignment-2/internal/http/datatransfers/inhouse"
-	"time"
 )
 
-type Notification struct {
-	Id      string    `json:"id"`
-	Country string    `json:"country"`
-	Event   string    `json:"event"`
-	Time    time.Time `json:"time"`
+type notificationResponse struct {
+	Id string `json:"id"`
 }
 
 // GetEndpointStructs returns the endpoints for the registrations handler. One with an ID and one without.
